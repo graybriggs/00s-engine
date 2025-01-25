726f243bab97ef2f77002b356bc407d05bf48014
Graham Briggs <grahambriggs@gmail.com>
2025-01-25 11:07:36 +0900

    needs an update

--- timer.h
+++ timer.h
@@ -1,31 +1,13 @@
 #pragma once
 
-#ifndef TIMER_H
-#define TIMER_H
-
 #include <chrono>
 
 struct Timer {
 
 	std::chrono::time_point<std::chrono::high_resolution_clock> time;
 
-	void start_timer() {
-		time = std::chrono::high_resolution_clock::now();
-	}
-
-	double current_time() {
-		auto end_time = std::chrono::high_resolution_clock::now();
-		auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - time);
-		return elapsed.count();
-	}
-
-	double end_timer() {
-		auto end_time = std::chrono::high_resolution_clock::now();
-		auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - time);
-		return elapsed.count();
-	}
+	void start_timer();
+	double current_time();
+	double end_timer();
 };
 
-
-
-#endif // TIMER_H
