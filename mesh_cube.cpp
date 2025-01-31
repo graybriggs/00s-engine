e7264c80d5132a7b433b0c9c09a766dbe34d8daa
Graham Briggs <grahambriggs@gmail.com>
2025-01-31 21:31:37 +0900

    so broken sadface

--- mesh_cube.cpp
+++ mesh_cube.cpp
@@ -1,49 +1,55 @@
-
-
-#include "mesh_cube.h"
+#include <iostream>
 
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GLFW/glfw3.h>
 
+#include "mesh_cube.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-MeshCube::MeshCube():
-	Mesh() {
+
+RMeshCube::RMeshCube() {
 
 	constexpr int CUBE_BUFFER_SIZE = 36 * 3;
 
 	const GLfloat cube_data[CUBE_BUFFER_SIZE] = {
-		-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+		-1.0f,-1.0f,-1.0f, 
 		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f, // triangle 1 : end
-		1.0f, 1.0f,-1.0f, // triangle 2 : begin
+		-1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f,-1.0f, 
 		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f, // triangle 2 : end
+		-1.0f, 1.0f,-1.0f, 
+		
 		1.0f,-1.0f, 1.0f,
 		-1.0f,-1.0f,-1.0f,
 		1.0f,-1.0f,-1.0f,
 		1.0f, 1.0f,-1.0f,
 		1.0f,-1.0f,-1.0f,
 		-1.0f,-1.0f,-1.0f,
+
 		-1.0f,-1.0f,-1.0f,
 		-1.0f, 1.0f, 1.0f,
 		-1.0f, 1.0f,-1.0f,
 		1.0f,-1.0f, 1.0f,
 		-1.0f,-1.0f, 1.0f,
 		-1.0f,-1.0f,-1.0f,
+
 		-1.0f, 1.0f, 1.0f,
 		-1.0f,-1.0f, 1.0f,
 		1.0f,-1.0f, 1.0f,
 		1.0f, 1.0f, 1.0f,
 		1.0f,-1.0f,-1.0f,
 		1.0f, 1.0f,-1.0f,
+
 		1.0f,-1.0f,-1.0f,
 		1.0f, 1.0f, 1.0f,
 		1.0f,-1.0f, 1.0f,
 		1.0f, 1.0f, 1.0f,
 		1.0f, 1.0f,-1.0f,
 		-1.0f, 1.0f,-1.0f,
+
 		1.0f, 1.0f, 1.0f,
 		-1.0f, 1.0f,-1.0f,
 		-1.0f, 1.0f, 1.0f,
@@ -52,6 +58,52 @@ MeshCube::MeshCube():
 		1.0f,-1.0f, 1.0f
 	};
 
+	const GLfloat cube_color[CUBE_BUFFER_SIZE] = {
+		0.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 0.0f,
+
+		0.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 0.0f,
+
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		0.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+
+
 	
     index_count = sizeof(cube_data);
 
@@ -59,11 +111,11 @@ MeshCube::MeshCube():
     glGenBuffers(1, &vbo);
 
 	glBindVertexArray(vao);
-
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cube_data), cube_data, GL_STATIC_DRAW);
 
     
+	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(
 			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
 			3,                  // size
@@ -72,5 +124,68 @@ MeshCube::MeshCube():
 			0,                  // stride
 			(void*)0            // array buffer offset
 	);
-	glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &vbo_col);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_col);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_color), cube_color, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(
+			1,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+	);
+
+	const GLfloat tex_coords[] = {
+		 0.0,  0.0,
+		 1.0,  0.0,
+		 0.0,  1.0,
+		 1.0,  1.0
+	};
+
+	int width;
+	int height;
+	int nrChannels;
+	unsigned char* data = stbi_load("./img/earth1.png", &width, &height, &nrChannels, 0);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Error generating texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+}
+
+GLuint RMeshCube::get_index_count() const {
+	return index_count;
+}
+
+void RMeshCube::bind() const {
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//glBindBuffer(GL_ARRAY_BUFFER, vbo_col);
+}
+
+void RMeshCube::unbind() const {
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void RMeshCube::cleanup() const {
+    
 }
