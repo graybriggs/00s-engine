272b46a7e1881f1913832350125a845cd93320ca
Graham Briggs <grahambriggs@gmail.com>
2025-06-17 21:55:12 +0900

    refactor

--- input.cpp
+++ input.cpp
@@ -6,6 +6,15 @@ Input::Input()
     left_mouse_click = false;
 }
 
+
+void Input::set_mouse_cursor(double x, double y) {
+    cursor_pos = {x, y};
+}
+
+mouse_cursor_pos Input::get_cursor_pos() {
+    return { cursor_pos.xpos, cursor_pos.ypos };
+}
+
 void Input::set_left_mouse_click() {
     left_mouse_click = true;
 }
@@ -18,10 +27,3 @@ bool Input::get_left_mouse_button_state() {
     return left_mouse_click;
 }
 
-void Input::set_mouse_cursor(double x, double y) {
-    cursor_pos = {x, y};
-}
-
-mouse_cursor_pos Input::get_cursor_pos() {
-    return { cursor_pos.xpos, cursor_pos.ypos };
-}
