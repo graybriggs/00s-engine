247d48b19515b2a73ddcaeeb546f3cd502525287
Graham Briggs <grahambriggs@gmail.com>
2025-07-02 18:15:57 +0900

    cube geometry plus uvs

--- mesh_cube.cpp
+++ mesh_cube.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <GL/glew.h>
 #include <GL/gl.h>
@@ -6,7 +7,6 @@
 #include <GLFW/glfw3.h>
 
 #include "mesh_cube.h"
-#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 
@@ -15,102 +15,174 @@ RMeshCube::RMeshCube() {
 	constexpr int CUBE_BUFFER_SIZE = 36 * 3;
 
 	const GLfloat cube_data[CUBE_BUFFER_SIZE] = {
-		-1.0f,-1.0f,-1.0f, 
-		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f, 
-		1.0f, 1.0f,-1.0f, 
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f, 
-		
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
+		// -1.0f,-1.0f,-1.0f, 
+		// -1.0f,-1.0f, 1.0f,
+		// -1.0f, 1.0f, 1.0f, 
+		// 1.0f, 1.0f,-1.0f, 
+		// -1.0f,-1.0f,-1.0f,
+		// -1.0f, 1.0f,-1.0f, 
 
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
+		// 1.0f,-1.0f, 1.0f,
+		// -1.0f,-1.0f,-1.0f,
+		// 1.0f,-1.0f,-1.0f,
+		// 1.0f, 1.0f,-1.0f,
+		// 1.0f,-1.0f,-1.0f,
+		// -1.0f,-1.0f,-1.0f, 
 
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
+		// -1.0f,-1.0f,-1.0f,
+		// -1.0f, 1.0f, 1.0f,
+		// -1.0f, 1.0f,-1.0f,
+		// 1.0f,-1.0f, 1.0f,
+		// -1.0f,-1.0f, 1.0f,
+		// -1.0f,-1.0f,-1.0f,
 
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
+		// -1.0f, 1.0f, 1.0f,
+		// -1.0f,-1.0f, 1.0f,
+		// 1.0f,-1.0f, 1.0f,
+		// 1.0f, 1.0f, 1.0f,
+		// 1.0f,-1.0f,-1.0f,
+		// 1.0f, 1.0f,-1.0f,
 
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f
+		// 1.0f,-1.0f,-1.0f,
+		// 1.0f, 1.0f, 1.0f,
+		// 1.0f,-1.0f, 1.0f,
+		// 1.0f, 1.0f, 1.0f,
+		// 1.0f, 1.0f,-1.0f,
+		// -1.0f, 1.0f,-1.0f,
+
+		// 1.0f, 1.0f, 1.0f,
+		// -1.0f, 1.0f,-1.0f,
+		// -1.0f, 1.0f, 1.0f,
+		// 1.0f, 1.0f, 1.0f,
+		// -1.0f, 1.0f, 1.0f,
+		// 1.0f,-1.0f, 1.0f
+		    // Front face
+    -0.5f, -0.5f,  0.5f,     // Bottom-left
+     0.5f, -0.5f,  0.5f,     // Bottom-right
+     0.5f,  0.5f,  0.5f,     // Top-right
+    -0.5f,  0.5f,  0.5f,     // Top-left
+
+    // Back face
+    -0.5f, -0.5f, -0.5f,   
+     0.5f, -0.5f, -0.5f,   
+     0.5f,  0.5f, -0.5f,   
+    -0.5f,  0.5f, -0.5f,   
+
+    // Left face
+    -0.5f, -0.5f, -0.5f,   
+    -0.5f, -0.5f,  0.5f,   
+    -0.5f,  0.5f,  0.5f,   
+    -0.5f,  0.5f, -0.5f,   
+
+    // Right face
+     0.5f, -0.5f, -0.5f,   
+     0.5f, -0.5f,  0.5f,   
+     0.5f,  0.5f,  0.5f,   
+     0.5f,  0.5f, -0.5f,   
+
+    // Top face
+    -0.5f,  0.5f,  0.5f,   
+     0.5f,  0.5f,  0.5f,   
+     0.5f,  0.5f, -0.5f,   
+    -0.5f,  0.5f, -0.5f,   
+
+    // Bottom face
+    -0.5f, -0.5f,  0.5f,   
+     0.5f, -0.5f,  0.5f,   
+     0.5f, -0.5f, -0.5f,   
+    -0.5f, -0.5f, -0.5f   
 	};
 
-	const GLfloat cube_color[CUBE_BUFFER_SIZE] = {
-		0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 1.0f,
-		0.0f, 1.0f, 0.0f,
+	// const GLfloat cube_color[CUBE_BUFFER_SIZE] = {
+	// 	0.0f, 1.0f, 0.0f,
+	// 	1.0f, 1.0f, 0.0f,
+	// 	1.0f, 0.0f, 0.0f,
+	// 	1.0f, 0.0f, 0.0f,
+	// 	0.0f, 1.0f, 1.0f,
+	// 	0.0f, 1.0f, 0.0f,
 
-		0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 1.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	1.0f, 1.0f, 1.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 1.0f,
+	// 	1.0f, 1.0f, 0.0f,
 
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
 
-		0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 0.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 0.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
 
-		0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	1.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	1.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	1.0f, 1.0f, 0.0f,
 
-		0.0f, 1.0f, 1.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 1.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 1.0f,
-		0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 1.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 1.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// 	0.0f, 1.0f, 1.0f,
+	// 	0.0f, 1.0f, 0.0f,
+	// };
+
+	const GLfloat tex_coords[] = {
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f
 	};
 
-
+	//GLuint indices[] = {
+	std::vector<GLuint> idx = {
+		0, 1, 2,  2, 3, 0,        // Front
+		4, 5, 6,  6, 7, 4,        // Back
+		8, 9,10, 10,11, 8,        // Left
+		12,13,14, 14,15,12,        // Right
+		16,17,18, 18,19,16,        // Top
+		20,21,22, 22,23,20         // Bottom
+	};
+	//indices = std::move(idx);
+	indices = idx;
 
 	
-    index_count = sizeof(cube_data);
+    index_count = sizeof(cube_data) / sizeof(float);
 
     glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
-
 	glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cube_data), cube_data, GL_STATIC_DRAW);
 
@@ -125,59 +197,49 @@ RMeshCube::RMeshCube() {
 			(void*)0            // array buffer offset
 	);
 
-    glGenBuffers(1, &vbo_col);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_col);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_color), cube_color, GL_STATIC_DRAW);
+    // glGenBuffers(1, &vbo_col);
+    // glBindBuffer(GL_ARRAY_BUFFER, vbo_col);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(cube_color), cube_color, GL_STATIC_DRAW);
 
+	// glEnableVertexAttribArray(1);
+	// glVertexAttribPointer(
+	// 		1,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+	// 		3,                  // size
+	// 		GL_FLOAT,           // type
+	// 		GL_FALSE,           // normalized?
+	// 		0,                  // stride
+	// 		(void*)0            // array buffer offset
+	// );
+
+
+
+
+	glGenBuffers(1, &tex_uv);
+	glBindBuffer(GL_ARRAY_BUFFER, tex_uv);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(tex_coords), tex_coords, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(
-			1,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
+			1,                  // attribute 0. 
+			2,                  // size
 			GL_FLOAT,           // type
 			GL_FALSE,           // normalized?
 			0,                  // stride
 			(void*)0            // array buffer offset
 	);
 
-	const GLfloat tex_coords[] = {
-		 0.0,  0.0,
-		 1.0,  0.0,
-		 0.0,  1.0,
-		 1.0,  1.0
-	};
+	GLuint element_buffer;
+	glGenBuffers(1, &element_buffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
 
-	int width;
-	int height;
-	int nrChannels;
-	unsigned char* data = stbi_load("./img/earth1.png", &width, &height, &nrChannels, 0);
-
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else {
-		std::cout << "Error generating texture" << std::endl;
-	}
-	stbi_image_free(data);
-
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 }
 
 GLuint RMeshCube::get_index_count() const {
-	return index_count;
+	//return index_count;
+	return indices.size();
 }
 
 void RMeshCube::bind() const {
-	glBindTexture(GL_TEXTURE_2D, texture);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	//glBindBuffer(GL_ARRAY_BUFFER, vbo_col);
 }
--- mesh_cube.h
+++ mesh_cube.h
@@ -1,9 +1,25 @@
 #pragma once
 
-#include "mesh.h"
+#include "renderable_mesh.h"
 
-class MeshCube : public Mesh {
+#include <vector>
+
+class RMeshCube : public RMesh {
 public:
-    MeshCube();
+    RMeshCube();
+
+    GLuint get_index_count() const override;
+    void bind() const override;
+    void unbind() const override;
+    void cleanup() const override;
+
 private:
+    std::vector<GLuint> indices;
+    GLuint index_count;
+
+    GLuint vao;
+    GLuint vbo;
+    GLuint vbo_col;
+    GLuint texture;
+    GLuint tex_uv;
 };
