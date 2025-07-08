1bf7b6c6b0146d0b0bab4099bdd3923688b76109
Graham Briggs <grahambriggs@gmail.com>
2025-07-08 23:18:42 +0900

    entity update

--- entity.cpp
+++ entity.cpp
@@ -1,13 +1,15 @@
 
 #include "entity.h"
 
-Entity::Entity(const Mesh* m, Material& mat, glm::mat4& model):
+Entity::Entity(const RMesh* m, const Material mat, const glm::mat4 model, GLuint tex, EntityType et):
     mesh(m),
     material(mat),
-    model(model)
+    model(model),
+    texture(tex),
+    entity_type(et)
 {}
 
-const Mesh* Entity::get_mesh() const {
+const RMesh* Entity::get_mesh() const {
     return mesh;
 }
 
@@ -17,4 +19,20 @@ Material Entity::get_material() const {
 
 glm::mat4 Entity::get_model_matrix() const {
     return model;
+}
+
+void Entity::set_model_matrix(glm::mat4 mm) {
+    model = mm;
+}
+
+void Entity::set_texture(GLuint tex) {
+    texture = tex;
+}
+
+GLuint Entity::get_texture() const {
+    return texture;
+}
+
+EntityType Entity::get_type() const {
+    return entity_type;
 }
--- entity.h
+++ entity.h
@@ -1,23 +1,45 @@
 #pragma once
 
-#include "mesh.h"
+#include "renderable_mesh.h"
 #include "material.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 
+
+enum class EntityType { UNTEXTURED_MODEL, TEXTURED_MODEL, LIGHT, LINE };
+
+namespace {
+	constexpr int UNTEXTURED = 0;
+	constexpr int LIGHT = 1;
+};
+
 class Entity {
 public:
 
-	Entity(const Mesh* m, Material& mat, glm::mat4& model);
+	//Entity(const RMesh* m, const Material mat, const glm::mat4 model, GLuint tex);
+	Entity(const RMesh* m, const Material mat, const glm::mat4 model, GLuint tex, EntityType et);
 
-	const Mesh* get_mesh() const;
+	const RMesh* get_mesh() const;
 	Material get_material() const;
 	glm::mat4 get_model_matrix() const;
+	void set_model_matrix(glm::mat4 mm);
+	void set_texture(GLuint tex);
+	GLuint get_texture() const;
+	EntityType get_type() const;
 
 private:
-	const Mesh* mesh;
+	const RMesh* mesh;
+	// multiple materials that can be changed?
 	Material material;
-	glm::mat4& model;
+	glm::mat4 model;
+
+	GLuint texture; // ?
+
+	EntityType entity_type;
+	
+	// Texture texture;??
+	// Sampler sampler;??
+
 };
