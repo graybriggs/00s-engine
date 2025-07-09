3f97fd53418f669b779fa6713e7f87b4aa6cd723
Graham Briggs <grahambriggs@gmail.com>
2025-07-09 19:57:12 +0900

    moved matrix operations into Entity for ease of use

--- entity.cpp
+++ entity.cpp
@@ -1,12 +1,15 @@
 
 #include "entity.h"
+#include "texture.h"
 
-Entity::Entity(const RMesh* m, const Material mat, const glm::mat4 model, GLuint tex, EntityType et):
+//Entity::Entity(const RMesh* m, const Material mat, const glm::mat4 model, Texture tex, EntityType et):
+Entity::Entity(const RMesh* m, const Material mat, Texture tex, EntityType et):
     mesh(m),
     material(mat),
-    model(model),
+    model(glm::mat4(1.0)),
     texture(tex),
-    entity_type(et)
+    entity_type(et),
+    viewable(true)
 {}
 
 const RMesh* Entity::get_mesh() const {
@@ -15,6 +18,10 @@ const RMesh* Entity::get_mesh() const {
 
 Material Entity::get_material() const {
     return material;
+}
+
+void Entity::set_material(Material mat) {
+    material = mat;
 }
 
 glm::mat4 Entity::get_model_matrix() const {
@@ -29,10 +36,26 @@ void Entity::set_texture(GLuint tex) {
     texture = tex;
 }
 
-GLuint Entity::get_texture() const {
+Texture Entity::get_texture() const {
     return texture;
 }
 
 EntityType Entity::get_type() const {
     return entity_type;
+}
+
+void Entity::set_viewable_flag(const bool b) {
+    viewable = b;
+}
+
+void Entity::scale(glm::vec3 factor) {
+    model = glm::scale(model, factor);
+}
+
+void Entity::translate(glm::vec3 units) {
+    model = glm::translate(model, units);
+}
+
+void Entity::rotate(float angle, glm::vec3 axis) {
+    model = glm::rotate(model, angle, axis);
 }
--- entity.h
+++ entity.h
@@ -2,6 +2,7 @@
 
 #include "./mesh/renderable_mesh.h"
 #include "material.h"
+#include "texture.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -19,29 +20,34 @@ class Entity {
 public:
 
 	//Entity(const RMesh* m, const Material mat, const glm::mat4 model, GLuint tex);
-	Entity(const RMesh* m, const Material mat, const glm::mat4 model, GLuint tex, EntityType et);
+	//Entity(const RMesh* m, const Material mat, const glm::mat4 model, Texture tex, EntityType et);
+	Entity(const RMesh* m, const Material mat, Texture tex, EntityType et);
 
-	const RMesh* get_mesh() const;
-	Material get_material() const;
 	void set_material(Material mat);
-	glm::mat4 get_model_matrix() const;
 	void set_model_matrix(glm::mat4 mm);
 	void set_texture(GLuint tex);
-	GLuint get_texture() const;
-	EntityType get_type() const;
 	void set_viewable_flag(const bool b);
+	
+	const RMesh* get_mesh() const;
+	Material get_material() const;
+	glm::mat4 get_model_matrix() const;
+	Texture get_texture() const;
+	EntityType get_type() const;
 
+	void scale(glm::vec3 factor);
+	void translate(glm::vec3 units);
+	void rotate(float, glm::vec3 axis);
 private:
 	const RMesh* mesh;
 	// multiple materials that can be changed?
 	Material material;
 	glm::mat4 model;
 
-	GLuint texture; // ?
+	//GLuint texture; // ?
 
 	EntityType entity_type;
 	
-	// Texture texture;??
+	Texture texture;
 	// Sampler sampler;??
 
 	bool viewable;
