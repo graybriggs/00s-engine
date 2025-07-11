219bf8da4139cff3c556d60fb4810a3b671ced84
Graham Briggs <grahambriggs@gmail.com>
2025-07-11 13:24:40 +0900

    lights in separate container

--- scene.cpp
+++ scene.cpp
@@ -1,5 +1,9 @@
 #include "scene.h"
 
-void Scene::add(Entity* ent) {
-    entities.emplace_back(ent);
+void Scene::add_entity(Entity* entity) {
+    entities.emplace_back(entity);
+}
+
+void Scene::add_light(Light* light) {
+    lights.emplace_back(light);
 }
--- scene.h
+++ scene.h
@@ -3,11 +3,14 @@
 #include <vector>
 
 #include "entity.h"
+#include "light.h"
 
 struct Scene {
 
-    void add(Entity* ent);
+    void add_entity(Entity* entity);
+    void add_light(Light* light);
 
     // expand to have filtering; ordering
     std::vector<Entity*> entities;
+    std::vector<Light*> lights;
 };
