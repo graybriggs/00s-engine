2f5e28b1c274d402318199dbca9a1abecf577247
Graham Briggs <grahambriggs@gmail.com>
2025-07-15 12:45:51 +0900

    get functions

--- scene.cpp
+++ scene.cpp
@@ -7,3 +7,10 @@ void Scene::add_entity(Entity* entity) {
 void Scene::add_light(Light* light) {
     lights.emplace_back(light);
 }
+
+std::vector<Entity*> Scene::get_entities() {
+    return entities;
+}
+std::vector<Light*> Scene::get_lights() {
+    return lights;
+}
--- scene.h
+++ scene.h
@@ -10,6 +10,9 @@ struct Scene {
     void add_entity(Entity* entity);
     void add_light(Light* light);
 
+    std::vector<Entity*> get_entities();
+    std::vector<Light*> get_lights();
+
     // expand to have filtering; ordering
     std::vector<Entity*> entities;
     std::vector<Light*> lights;
