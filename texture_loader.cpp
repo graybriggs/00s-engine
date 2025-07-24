d211a2f383ef4bfe03b8961f3c7b8acd8b1601b0
Graham Briggs <grahambriggs@gmail.com>
2025-07-24 22:57:43 +0900

    const&

--- texture_loader.cpp
+++ texture_loader.cpp
@@ -1,39 +1,56 @@
 
+#include "texture.h"
 #include "texture_loader.h"
 #include "stb_image.h"
-
-#include <iostream>
 
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GLFW/glfw3.h>
 
-GLuint load_texture(std::string path) {
+#include <iostream>
+#include <string_view>
+
+Texture TextureManager::load_texture(const std::string& name, const std::string& path) {
 
 	int width;
 	int height;
 	int nbr_channels;
-	unsigned char* data = stbi_load(path.c_str(), &width, &height, &nbr_channels, 0);
+	unsigned char* data = stbi_load(path.data(), &width, &height, &nbr_channels, 0);
 
     GLuint texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	if (data) {
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
+
+		std::cout << "Generated texture: `" << name << "` id: " << texture << "\n";
+		//textures[name] = Texture(texture);
 	}
 	else {
 		std::cout << "Error generating texture" << std::endl;
-        return -1;
+        return 0;
 	}
 	stbi_image_free(data);
 
     return texture;
+}
+
+Texture TextureManager::get_texture(const std::string& name) const {
+
+	auto it = textures.find(name.data());
+	if (it != std::end(textures)) {
+		return it->second;
+	}
+	else {
+		return 0;
+	}
+	//return 0;
 }
--- texture_loader.h
+++ texture_loader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "texture.h"
+
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
@@ -8,13 +10,13 @@
 #include <string>
 #include <unordered_map>
 
-
 class TextureManager {
 public:
     TextureManager() = default;
 
-    GLuint load_texture(std::string name, std::string path);
-    GLuint get_texture(std::string name);
+    Texture load_texture(const std::string& name, const std::string& path);
+    Texture get_texture(const std::string& name) const;
 private:
-    std::unordered_map<std::string, GLuint> textures;
+    //std::unordered_map<std::string, GLuint> textures;
+    std::unordered_map<std::string, Texture> textures;
 };
