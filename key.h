9e90d96b1884c251bbd43fff0df695ab6f97d1aa
Graham Briggs <grahambriggs@gmail.com>
2025-08-12 15:13:28 +0900

    inline constexpr keys

--- key.h
+++ key.h
@@ -11,10 +11,56 @@ enum class KeyPress {
     NUM7,
     NUM8,
     NUM9,
-    A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,W,X,Y,Z,
+    A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,
     UP,
     DOWN,
     LEFT,
     RIGHT
 };
 }
+
+
+//#define KP_E static_cast<int>(Key::KeyPress::E)
+
+inline constexpr int Key_NUM0 = static_cast<int>(Key::KeyPress::NUM0);
+inline constexpr int Key_NUM1 = static_cast<int>(Key::KeyPress::NUM1);
+inline constexpr int Key_NUM2 = static_cast<int>(Key::KeyPress::NUM2);
+inline constexpr int Key_NUM3 = static_cast<int>(Key::KeyPress::NUM3);
+inline constexpr int Key_NUM4 = static_cast<int>(Key::KeyPress::NUM4);
+inline constexpr int Key_NUM5 = static_cast<int>(Key::KeyPress::NUM5);
+inline constexpr int Key_NUM6 = static_cast<int>(Key::KeyPress::NUM6);
+inline constexpr int Key_NUM7 = static_cast<int>(Key::KeyPress::NUM7);
+inline constexpr int Key_NUM8 = static_cast<int>(Key::KeyPress::NUM8);
+inline constexpr int Key_NUM9 = static_cast<int>(Key::KeyPress::NUM9);
+
+inline constexpr int Key_A = static_cast<int>(Key::KeyPress::A);
+inline constexpr int Key_B = static_cast<int>(Key::KeyPress::B);
+inline constexpr int Key_C = static_cast<int>(Key::KeyPress::C);
+inline constexpr int Key_D = static_cast<int>(Key::KeyPress::D);
+inline constexpr int Key_E = static_cast<int>(Key::KeyPress::E);
+inline constexpr int Key_F = static_cast<int>(Key::KeyPress::F);
+inline constexpr int Key_G = static_cast<int>(Key::KeyPress::G);
+inline constexpr int Key_H = static_cast<int>(Key::KeyPress::H);
+inline constexpr int Key_I = static_cast<int>(Key::KeyPress::I);
+inline constexpr int Key_J = static_cast<int>(Key::KeyPress::J);
+inline constexpr int Key_K = static_cast<int>(Key::KeyPress::K);
+inline constexpr int Key_L = static_cast<int>(Key::KeyPress::L);
+inline constexpr int Key_M = static_cast<int>(Key::KeyPress::M);
+inline constexpr int Key_N = static_cast<int>(Key::KeyPress::N);
+inline constexpr int Key_O = static_cast<int>(Key::KeyPress::O);
+inline constexpr int Key_P = static_cast<int>(Key::KeyPress::P);
+inline constexpr int Key_Q = static_cast<int>(Key::KeyPress::Q);
+inline constexpr int Key_R = static_cast<int>(Key::KeyPress::R);
+inline constexpr int Key_S = static_cast<int>(Key::KeyPress::S);
+inline constexpr int Key_T = static_cast<int>(Key::KeyPress::T);
+inline constexpr int Key_U = static_cast<int>(Key::KeyPress::U);
+inline constexpr int Key_V = static_cast<int>(Key::KeyPress::V);
+inline constexpr int Key_W = static_cast<int>(Key::KeyPress::W);
+inline constexpr int Key_X = static_cast<int>(Key::KeyPress::X);
+inline constexpr int Key_Y = static_cast<int>(Key::KeyPress::Y);
+inline constexpr int Key_Z = static_cast<int>(Key::KeyPress::Z);
+
+inline constexpr int Key_UP = static_cast<int>(Key::KeyPress::UP);
+inline constexpr int Key_DOWN = static_cast<int>(Key::KeyPress::DOWN);
+inline constexpr int Key_LEFT = static_cast<int>(Key::KeyPress::LEFT);
+inline constexpr int Key_RIGHT = static_cast<int>(Key::KeyPress::RIGHT);
